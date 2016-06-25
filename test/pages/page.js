64b35a8266b6dead5890ba569696e4f1bc01044e
Nikitha Iyer <nikita9.iyer@gmail.com>
2016-06-25 11:47:32 +0530

    Adding loginIfRequired to demonstrate non angular interaction.

--- test/pages/page.js
+++ test/pages/page.js
@@ -3,8 +3,22 @@
 var configuration = require(utilsDir + '/configuration');
 
 var page = {
+    loginIfRequired: function () {
+        var userNameElement = element(by.id("username"));
+        userNameElement.isPresent().then(function(isPresent) {
+            if (isPresent) {
+                userNameElement.clear().sendKeys(configuration.get("username"));
+                element(by.id("pwd")).clear().sendKeys(configuration.get("password"));
+                element(by.buttonText("Login")).click();
+            }
+        })
+    },
+
     visit: function (url) {
+        browser.ignoreSynchronization = true;
         browser.get(configuration.get('baseUrl') + url);
+        this.loginIfRequired();
+        browser.ignoreSynchronization = false;
         browser.waitForAngular();
     },
 
